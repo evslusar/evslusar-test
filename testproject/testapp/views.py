ff3e28b6250757de8681c1713958cbe6fcf87659
evslusar <evslusar@gmail.com>
2010-09-01 17:41:04 +0300

    ticket:1
    Unit tests added. Ticket completed

--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -6,3 +6,4 @@ def default_person_info():
     default_person = Person.objects.get(firstname__iexact = 'Evgeniy', lastname__iexact = 'Slusar')
     info = { 'queryset': Person.objects.all(), 'object_id': default_person.pk, 'template_name': 'person_detail.html' }
     return info
+
