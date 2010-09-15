987546a93ae94143713911b432664a474b09b49a
evslusar <evslusar@gmail.com>
2010-09-15 15:06:29 +0300

    ticket:4
    - pep8
    - unicode
    - removed code from 5
    - templates
    - contextproc to separate file

--- testproject/testapp/tests.py
+++ testproject/testapp/tests.py
@@ -1,21 +1,15 @@
-
-
 from django.test import TestCase
-
-from testapp.models import Person
-from testapp.views import default_person_info
-from testapp.views import default_person
-
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import QueryDict
-from testapp.models import HttpRequestLog
+from django.conf import settings
+
+from testapp.models import Person, HttpRequestLog
+from testapp.views import default_person_info, default_person
 
 
 class PersonModelTest(TestCase):
 
     def test_initial_data(self):
-
-        # test initial data loaded
         person = default_person()
         self.assertTrue(person)
         self.assertEqual(person.firstname, "Evgeniy")
@@ -31,16 +25,15 @@ class DefaultPersonInfoTest(TestCase):
         self.assertEqual(info['template_name'], "person_detail.html")
         self.assertTrue(info['queryset'])
 
-        default_person = info['queryset'].get(pk = info['object_id'])
+        default_person = info['queryset'].get(pk=info['object_id'])
         self.assertEqual(default_person.firstname, "Evgeniy")
         self.assertEqual(default_person.lastname, "Slusar")
 
 
 class HttpRequestLogTest(TestCase):
 
-
     def get_log_item(self, path):
-        return HttpRequestLog.objects.get(path__iexact = path)
+        return HttpRequestLog.objects.get(path__iexact=path)
 
     def test_request_log(self):
         test_path = '/'
@@ -58,47 +51,9 @@ class HttpRequestLogTest(TestCase):
         self.assertEqual(params['b'], '2')
 
 
-class PersonEditTest(TestCase):
-
-    def test_person_edit(self):
-        params = { 'firstname': 'Evgeniy', 'lastname': 'Slusar', 'email': 'abs@gmail.com', 'phone': '0000000000', 'biography': 'bio' }
- 
-        self.client.post('/edit/', params)
-        dp = default_person()
-        self.assertEqual(dp.firstname, params['firstname'])
-        self.assertEqual(dp.lastname, params['lastname'])
-        self.assertEqual(dp.email, params['email'])
-        self.assertEqual(dp.phone, params['phone'])
-        self.assertEqual(dp.biography, params['biography'])
-
-
-
-from django.conf import settings
-
 class ContextProcTest(TestCase):
 
-    def test_context_proc(self):    
+    def test_context_proc(self):
         response = self.client.get('/settings/')
         context_settings = response.context['settings']
         self.assertEqual(settings, context_settings)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
--- testproject/testapp/views.py
+++ testproject/testapp/views.py
@@ -1,54 +1,29 @@
-# Create your views here.
-
-from testapp.models import *
-
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
+
+from testapp.models import *
+from testapp.contextprocs import settings_context_proc
+
 
 def default_person():
     return Person.objects.get(pk=1)
 
+
 def default_person_info():
     p = default_person()
-    info = { 'queryset': Person.objects.all(), 'object_id': p.pk, 'template_name': 'person_detail.html' }
+    info = {'queryset': Person.objects.all(),
+        'object_id': p.pk,
+        'template_name': 'person_detail.html'}
     return info
 
 
 def request_log_info():
-    return { 'queryset': HttpRequestLog.objects.all().order_by('-request_date'), 'template_name': 'request_log_list.html' }
+    all_requests = HttpRequestLog.objects.all()
+    return {'queryset': all_requests.order_by('-request_date'),
+        'template_name': 'request_log_list.html'}
 
-
-def edit_view(request):
-    form = PersonForm()
-    if request.method == 'GET':
-        form = PersonForm(instance=default_person())
-    elif request.method == 'POST':
-        form = PersonForm(request.POST, instance=default_person())
-        if form.is_valid(): 
-            form.save()
-            return HttpResponseRedirect('/')
-    return render_to_response('person_edit.html', {'form' : form})
-
-
-
-
-from django.conf import settings
-from django.template import RequestContext
-
-def settings_context_proc(request):
-    return {'settings': settings}
 
 def settings_view(request):
     context = RequestContext(request, {}, [settings_context_proc])
-    return render_to_response('view_settings.html', context_instance = context)
-
-
-
-
-
-
-
-
-
-
-
+    return render_to_response('view_settings.html', context_instance=context)
