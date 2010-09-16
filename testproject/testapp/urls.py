bcaa6dc4a097810173302d2805221cd6b7354bb0
evslusar <evslusar@gmail.com>
2010-09-16 16:41:11 +0300

    ticket:9
    previous corrections

--- testproject/testapp/urls.py
+++ testproject/testapp/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
 from django.contrib import admin
-
 from django.views.generic import list_detail
+
 from testapp.views import *
+
 
 admin.autodiscover()
 
@@ -12,13 +13,7 @@ urlpatterns = patterns('',
     (r'^log/$', list_detail.object_list, request_log_info()),
     (r'^edit/$', edit_view),
     (r'^settings/$', settings_view),
-    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    (r'^login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}),
     (r'^logout/$', logout_view),
-    (settings.ADMIN_URL_PATTERN, include(admin.site.urls)),
-)
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': './media/'}),
-    )
-
+    (settings.ADMIN_URL_PATTERN, include(admin.site.urls)))
